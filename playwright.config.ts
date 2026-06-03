e45c0593db675f683814f497eaa455611888a256
Rushi0703 <rushikesh.p@Brownboxconsulting.com>
2026-06-03 15:37:18 +0530

    refactor: remove unused screenshot option and clean up project configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,8 +30,6 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://developer.shell.com',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    screenshot: 'on',
     trace: 'retain-on-failure-and-retries',
   },
 
@@ -41,63 +39,30 @@ export default defineConfig({
     timeout: 10000, // Set default timeout for expect assertions
   },
 
-  /* Configure projects for major browsers */
-  // projects: [
-  //   {
-  //     name: 'chromium',
-  //     use: {
-  //       ...devices['Desktop Chrome'],
-  //       deviceScaleFactor: undefined,
-  //       viewport: null,
-  //       launchOptions: {
-  //         args: ['--start-maximized'],
-  //       },
-  //     }
+   projects: [
+    {
+      name: 'dummyjson-project',
+      testMatch: '**/auth-chain.spec.ts',
+      use: {
+        baseURL: 'https://dummyjson.com'
+      }
+    },
 
-  //   },
+    {
+      name: 'data-project',
+      testMatch: '**/data-brutal.spec.ts',
+      use: {
+        baseURL: 'https://dummyjson.com'
+      }
+    },
 
-    // {
-    //   name: 'firefox',
-    // use: {
-    //   ...devices['Desktop Firefox'],
-    //   deviceScaleFactor: undefined,
-    //   viewport: null,
-    //   launchOptions: {
-    //     args: ['--start-maximized'], // Start browser in maximized mode
-    //   },
-    // }
-    // },
+    {
+      name: 'chain-project',
+      testMatch: '**/multi-api-chain.spec.ts',
+      use: {
+        baseURL: 'https://dummyjson.com'
+      }
+    }
+  ]
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  // ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
